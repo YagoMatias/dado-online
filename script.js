02f497e003845c5b024c7acdf27eda6285d40772
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2023-01-17 15:04:55 -0300

    update adicionado numero de dados

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
-const quadrado = document.querySelector(".quadrado");
+// ======================== DADOS ==============================
+const quadrado = document.querySelectorAll(".quadrado");
 const display = document.querySelector("p");
 const botaoRodar = document.querySelector(".rodar");
 const botaoZerar = document.querySelector(".zerar");
 
 function rodar() {
-  const numeros = parseInt(Math.random() * (7 - 1) + 1);
-  quadrado.classList.add(`__${numeros}`);
-  // if (numeros === 1) {
-  //   quadrado.classList.add("__1");
-  // } else if (numeros === 2) {
-  //   quadrado.classList.add("__2");
-  // } else if (numeros === 3) {
-  //   quadrado.classList.add("__3");
-  // } else if (numeros === 4) {
-  //   quadrado.classList.add("__4");
-  // } else if (numeros === 5) {
-  //   quadrado.classList.add("__5");
-  // } else {
-  //   quadrado.classList.add("__6");
-  // }
-  quadrado.classList.toggle("rotate-center");
-  botaoRodar.setAttribute("disabled", "");
+  const sorteio = {
+    numero: (numeros1 = parseInt(Math.random() * (7 - 1) + 1)),
+    numero2: (numeros2 = parseInt(Math.random() * (7 - 1) + 1)),
+    numero3: (numeros2 = parseInt(Math.random() * (7 - 1) + 1)),
+  };
+  quadrado.forEach((item) => {
+    quadrado[0].classList.add(`__${sorteio.numero}`);
+    quadrado[1].classList.add(`__${sorteio.numero2}`);
+    quadrado[2].classList.add(`__${sorteio.numero3}`);
+    item.classList.toggle("rotate-center");
+    botaoRodar.setAttribute("disabled", "");
+  });
 }
 
 function apagar() {
-  quadrado.classList.remove(
-    "__1",
-    "__2",
-    "__3",
-    "__4",
-    "__5",
-    "__6",
-    "rotate-center"
-  );
+  quadrado.forEach((item) => {
+    item.classList.remove(
+      "__1",
+      "__2",
+      "__3",
+      "__4",
+      "__5",
+      "__6",
+      "rotate-center"
+    );
+  });
   botaoRodar.removeAttribute("disabled", "");
 }
 
 botaoRodar.addEventListener("click", rodar);
 botaoZerar.addEventListener("click", apagar);
+
+// =======================DROPDOWN==================================
+
+const dropdown = document.querySelector("h3");
+const listaDropdown = document.querySelector(".listaDropdown");
+
+function abrirDropdown() {
+  listaDropdown.classList.toggle("aberto");
+}
+
+dropdown.addEventListener("click", abrirDropdown);
+
+// =======================QUANTIDADE DE DADOS =====================
+
+const itemLista = document.querySelector(".listaDropdown");
+
+itemLista.addEventListener("click", (event) => {
+  if (event.target.matches("p")) {
+    const botao = event.target;
+    const action = botao.dataset.action;
+    if (!action) {
+      console.log("nada");
+    } else if (action === "primeiro") {
+      quadrado[1].classList.remove("doisDados");
+      quadrado[2].classList.remove("tresDados");
+      listaDropdown.classList.remove("aberto");
+    } else if (action === "segundo") {
+      quadrado[1].classList.add("doisDados");
+      quadrado[2].classList.remove("tresDados");
+      listaDropdown.classList.remove("aberto");
+    } else if (action === "terceiro") {
+      quadrado[2].classList.add("tresDados");
+      listaDropdown.classList.remove("aberto");
+    }
+  }
+});
